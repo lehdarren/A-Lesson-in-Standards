81e573e5cb3487509c0af88e6e2737e21146cd42
Mackenzie1215 <64882508+Mackenzie1215@users.noreply.github.com>
2021-10-23 13:51:37 -0400

    Update BreakingTheRules.java

--- src/BreakingTheRules.java
+++ src/BreakingTheRules.java
@@ -8,14 +8,24 @@ public class BreakingTheRules {
          * https://wiki.sei.cmu.edu/confluence/display/java/IDS03-J.+Do+not+log+unsanitized+user+input
          */
 
+     public String sanitize(String msg) {
+        Pattern newline = Pattern.compile("\n");
+        Matcher matcher = newline.matcher(msg);
+         return matcher.replaceAll("\n  ");
+         }
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Username: ");
         String username = scanner.nextLine();
+        sanatize(username);
 
+   
         System.out.println("Enter password: ");
         String password = scanner.nextLine();
-
+        sanatize(password);
+        
         System.out.println("Logging in with username: " + username + " and password: " + password);
+
 
         /**
          * Issue #2
@@ -23,7 +33,7 @@ public class BreakingTheRules {
          * https://wiki.sei.cmu.edu/confluence/display/java/NUM00-J.+Detect+or+prevent+integer+overflow
          */
 
-        byte sum;
+        int sum;
         int number = 100;
 
         sum = number + 28;
@@ -36,8 +46,8 @@ public class BreakingTheRules {
          * https://wiki.sei.cmu.edu/confluence/display/java/NUM09-J.+Do+not+use+floating-point+variables+as+loop+counters
          */
 
-        for(float x = 0.1f; x <= 1.5f; x += 0.15f) {
-            System.out.println("Current number: " + x);
+        for(int i = 0; i<100; i++) {
+            System.out.println("Current number: " + 1);
         }
     }
 }
